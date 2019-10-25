8c05b889ea1979112ed6cc38f5ae154bde1e4dba
mfvil <rarefyev@gmail.com>
2019-10-25 18:29:54 -0500

    0.0.5: Changed templates and info files.

--- src/pages/info.js
+++ src/pages/info.js
@@ -9,25 +9,12 @@ export default function Info() {
     <Layout page="info" bgColor={infoData.background_color}>
       <section className={infoStyles.info_blurb}>
         <h2>
-          <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
-          <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
+          Deep Poets blog
         </h2>
         <ul>
           <li>
             <p>
-              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
-                Twitter: @{infoData.contact.twitter_handle}
-              </a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
+              Created by Richard Rockland and friends/colleagues.
             </p>
           </li>
         </ul>
